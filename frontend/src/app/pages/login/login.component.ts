38c347fb77b7260eae60638648ce9a9d08493541
Thrasos <tnta728@gmail.com>
2024-12-20 13:38:14 +0700

    update version angular clicli

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,27 +1,27 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+  import { Component } from '@angular/core';
+  import { FormsModule } from '@angular/forms';
+  import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent {
-  username: string = '';
-  password: string = '';
+  @Component({
+    selector: 'app-login',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
+  })
+  export class LoginComponent {
+    username: string = '';
+    password: string = '';
 
-  constructor(private router: Router) {}
+    constructor(private router: Router) {}
 
-  onSubmit() {
-    console.log (this.username, this.password);
-    if (this.username === 'admin' && this.password === 'password') {
-      this.router.navigate(['/mainPage']);
-    } else {
-      alert('Invalid credentials');
+    onSubmit() {
+      console.log (this.username, this.password);
+      if (this.username === 'admin' && this.password === 'password') {
+        this.router.navigate(['/mainPage']);
+      } else {
+        alert('Invalid credentials');
+      }
     }
   }
-}
 
